a2ec8ead27e513568344a5d74c363a629929ba80
dayron.rafael <dayron.rafael@blueliv.com>
2021-06-25 08:29:21 -0400

    init: setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ContactModule } from './containers/contact/contact.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ContactModule],
   providers: [],
   bootstrap: [AppComponent],
 })
